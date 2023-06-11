410dead4c48843d83d7247609ec0d7a3dc78e9c9
Nguyen Quan <108479982+quannguyen0103@users.noreply.github.com>
2023-06-11 10:07:14 +0700

    Update 04_winner_ages.py

--- src/04_winner_ages.py
+++ src/04_winner_ages.py
@@ -1,20 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
-
 #1. import libraries and data
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-#import data
 data = pd.read_csv("dataset.csv")
-
-
-# In[4]:
-
 
 #2. clean and transform data
 data["birth_date"] = pd.to_datetime(data["birth_date"])
@@ -22,18 +14,10 @@ data["birth_year"] = data["birth_date"].dt.year
 data["win_prize_age"] = data["year"].astype(int) - data["birth_year"].astype(int)
 data = data.dropna(subset=["birth_year"])
 
-
-# In[5]:
-
-
 #3. select data to plot
 winners = data[["year", "win_prize_age"]]
 winners_age = winners.groupby("year").mean().reset_index()
 winners_age.columns = ["year","avg_age"]
-
-
-# In[ ]:
-
 
 #4. plot the data
 fig, ax = plt.subplots(figsize = (12, 8))
@@ -47,4 +31,3 @@ ax.set_title("Age of Nobel-Prize-Winner When receiving the Nobel Prize over time
 ax.set_xlabel("Year")
 ax.set_ylabel("Age")
 ax.grid(True)
-
