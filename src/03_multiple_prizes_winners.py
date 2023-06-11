e8321c7d65dd1f9398972383f833c74af4b4b07a
Nguyen Quan <108479982+quannguyen0103@users.noreply.github.com>
2023-06-11 10:06:08 +0700

    Update 03_multiple_prizes_winners.py

--- src/03_multiple_prizes_winners.py
+++ src/03_multiple_prizes_winners.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
 #1. Import library and data
 import pandas as pd
 data = pd.read_csv("dataset.csv")
-
-
-# In[ ]:
-
 
 #2. Get the list of scientists/writers/activists (names, and job) who win Nobel prize MORE THAN 1.
 data["category"].unique()
@@ -18,4 +11,3 @@ nobel_winners = data.loc[:, ["category", "full_name"]]
 nobel_winners = data.groupby(["full_name", "category"]).size().reset_index(name = "nobel prizes")
 names = nobel_winners.loc[nobel_winners["category"].isin(['Chemistry', 'Literature', 'Peace']) 
                           & (nobel_winners["nobel prizes"] > 1)]
-
